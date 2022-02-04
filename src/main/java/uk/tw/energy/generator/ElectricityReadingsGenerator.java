ae663bc07596006e7e6bcf46d90a9387802d5cff
Christian Barahona <barahonac2m@gmail.com>
2022-02-03 21:24:51 -0500

    Reduce tech deb

--- src/main/java/uk/tw/energy/generator/ElectricityReadingsGenerator.java
+++ src/main/java/uk/tw/energy/generator/ElectricityReadingsGenerator.java
@@ -23,11 +23,11 @@ public class ElectricityReadingsGenerator {
     public List<ElectricityReading> generate(int number) {
         List<ElectricityReading> readings = new ArrayList<>();
         Instant now = Instant.now();
-
-        for (int i = 0; i < number; i++) {
+        long limit = (long)number;
+        for (long i = 0; i < limit; i++) {
             double positiveRandomValue = Math.abs(readingRandomiser.nextGaussian());
             BigDecimal randomReading = BigDecimal.valueOf(positiveRandomValue).setScale(4, RoundingMode.CEILING);
-            ElectricityReading electricityReading = new ElectricityReading(now.minusSeconds(i * 10), randomReading);
+            ElectricityReading electricityReading = new ElectricityReading(now.minusSeconds( i * 10l), randomReading);
             readings.add(electricityReading);
         }
 
